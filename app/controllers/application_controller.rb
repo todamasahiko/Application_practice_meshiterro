c34a750b5aa9a4ac7bdfe416197add6072837619
todamasahiko <todaamasahiko@yahoo.co.jp>
2020-08-19 15:38:23 +0900

    [Finish5-7]

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+
+  before_action :authenticate_user!
+  before_action :configue_permitted_parameters, if: :devise_controller?
+
+protected
+  def configue_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '2d4c4b2cc9464bd19091ea0098f143bcc1ca4ff65ff34e4d06155b8ded541ce70f0e800bb2c68193ea3798ccad15d5f05d1821c7627c6369499138020c8a87c7'
